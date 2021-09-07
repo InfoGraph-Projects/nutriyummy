552c71e91f93d672f0a683b0b9aa39f96e891ee7
shadykh <eng.shady94@gmail.com>
2021-09-07 10:27:07 +0300

    Add_Login-functionality

--- nutriyummy/src/components/Login/Login.jsx
+++ nutriyummy/src/components/Login/Login.jsx
@@ -3,28 +3,49 @@
  */
 
 // 3rd Party Resources
-import React, { useRef } from "react";
+import React, { useContext, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router";
+import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
 
+// Internal Resources
+import "./login.css";
+import { AuthContext } from "../../context/authContext";
+
 // Login - Component
 export default function Login() {
-
+  const context = useContext(AuthContext);
   const username = useRef();
   const password = useRef();
   const history = useHistory();
-  };
 
+  // Function to hit the signin endpoint
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post(
+      "https://nutriyummy-backend.herokuapp.com/signin",
+      {},
+      {
+        auth: {
+          username: username.current.value,
+          password: password.current.value,
+        },
+      }
+    );
+
+    context.setUser(res.data);
+    history.push("/profile");
+  };
   return (
     <React.Fragment>
       <Container className="login">
         <img src="/assets/logo.png" alt="logo" style={{ width: "100px" }} />
         <br />
         <h5>Login Page</h5>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group
             style={{ padding: "0 1rem" }}
             className="mb-3"
@@ -37,6 +58,7 @@ export default function Login() {
               placeholder="Enter username"
             />
           </Form.Group>
+
           <Form.Group
             style={{ padding: "0 1rem" }}
             className="mb-3"
@@ -54,6 +76,7 @@ export default function Login() {
               Login
             </Button>
             <Button variant="success">
+              {" "}
               <Link to="/register">Register Now ?</Link>
             </Button>
           </div>
